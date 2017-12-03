34d508e3f590c0608f34e035f5a42b47fa4c4f08
Ar0xA <ar0xa@tldr.nu>
2017-12-03 13:21:24 +0100

    testing youtube blocking

--- update-adblock-dnsmasq.sh
+++ update-adblock-dnsmasq.sh
@@ -36,7 +36,7 @@ curl -s "http://sysctl.org/cameleon/hosts" "http://hosts-file.net/ad_servers.txt
 curl -s "https://raw.githubusercontent.com/notracking/hosts-blocklists/master/hostnames.txt" "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts" "http://winhelp2002.mvps.org/hosts.txt" "http://someonewhocares.org/hosts/zero/hosts" | grep -w ^0.0.0.0 | cut -c 9- | sed 's/\s\{1,\}.*//' >> $temp_ad_file
 
 #host only lists
-curl -s "http://mirror1.malwaredomains.com/files/justdomains" "https://zeustracker.abuse.ch/blocklist.php?download=domainblocklist" "https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt" "https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt"| grep -v "#" >> $temp_ad_file
+curl -s "https://raw.githubusercontent.com/Ar0xA/USG-DNS-ADBLOCK/master/youtube-google.txt" "http://mirror1.malwaredomains.com/files/justdomains" "https://zeustracker.abuse.ch/blocklist.php?download=domainblocklist" "https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt" "https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt"| grep -v "#" >> $temp_ad_file
 
 # remove the carriage return at the end of each line of the temporary file, and convert it into a Dnsmasq format
 sed -i -e 's/\r$//; s:.*:address=/&/0\.0\.0\.0:' $temp_ad_file
